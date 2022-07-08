63def767adb256d36d0f06f1f149f9376e625cd9
Bomme <94340274+Bommmee@users.noreply.github.com>
2022-07-08 09:38:10 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import pip
+import sys
 st.text('hello streamlit! ')
 st.text(pip --version)
+st.text(sys.version)
+pip insatll pip --upgrade 
